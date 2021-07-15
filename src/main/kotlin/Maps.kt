91552b157ed9719af7efcaaa4e304509b3aa1ac7
jailsonsf <jailson.s.f@outlook.com>
2021-07-15 19:45:19 -0300

    feat: Update Add Maps

---
+++ src/main/kotlin/Maps.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val pair: Pair<String, Double> = Pair("José", 1500.0)
+    val map1 = mapOf(pair)
+    map1.forEach { (k, v) -> println("Chave: $k - Valor: $v") }
+
+    val map2 = mapOf(
+        "Pedro" to 2000.0,
+        "Elisa" to 2500.0
+    )
+    map2.forEach { (k, v) -> println("Chave: $k - Valor: $v") }
+}
